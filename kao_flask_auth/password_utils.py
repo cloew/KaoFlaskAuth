90e125045f7f5696e61295bbad2afedd94a03235
Chris Loew <cloew123@gmail.com>
2015-10-31 11:51:18 -0600

    Fixed bad variable reference

--- kao_flask_auth/password_utils.py
+++ kao_flask_auth/password_utils.py
@@ -1,7 +1,7 @@
 from passlib.apps import custom_app_context
 
 def make_password(raw_password):
-    return pwd_context.encrypt(raw_password)
+    return custom_app_context.encrypt(raw_password)
 
 def check_password(raw_password, enc_password):
-    return pwd_context.verify(raw_password, enc_password)
+    return custom_app_context.verify(raw_password, enc_password)
