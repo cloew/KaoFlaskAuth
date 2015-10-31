1cf3c8e1cb50a1693f464ed6d4a2612a56dbce85
Chris Loew <cloew123@gmail.com>
2015-10-31 11:50:44 -0600

    Switched to passlib for pasword encoding and checking

--- kao_flask_auth/password_utils.py
+++ kao_flask_auth/password_utils.py
@@ -1,16 +1,7 @@
-import random
-from hashlib import sha1
-
-# borrowing these methods, slightly modified, from flask-peewee which in turn borrowed from django.contrib.auth
-def get_hexdigest(salt, raw_password):
-    data = salt + raw_password
-    return sha1(data.encode('utf8')).hexdigest()
+from passlib.apps import custom_app_context
 
 def make_password(raw_password):
-    salt = get_hexdigest(str(random.random()), str(random.random()))[:5]
-    hsh = get_hexdigest(salt, raw_password)
-    return '%s$%s' % (salt, hsh)
+    return pwd_context.encrypt(raw_password)
 
 def check_password(raw_password, enc_password):
-    salt, hsh = enc_password.split('$', 1)
-    return hsh == get_hexdigest(salt, raw_password)
+    return pwd_context.verify(raw_password, enc_password)
